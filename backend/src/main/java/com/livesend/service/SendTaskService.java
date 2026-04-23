b62dbdb703ebe500fbcbc6afef3591b10f12116c
jian <740928120@qq.com>
2026-04-23 14:59:49 +0800

    移除编译产物，应用 .gitignore

--- backend/src/main/java/com/livesend/service/SendTaskService.java
+++ backend/src/main/java/com/livesend/service/SendTaskService.java
@@ -3,6 +3,7 @@ package com.livesend.service;
 import com.livesend.entity.*;
 import com.livesend.repository.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
